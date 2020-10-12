129b9675209648ae483228690ddcd2f5e6e0875e
Hasan Hasanov <hasanovh14@gmail.com>
2020-10-12 20:21:55 +0200

    Conditional Statements

--- 02.conditionalStatements/12.tradeCommissions.js
+++ 02.conditionalStatements/12.tradeCommissions.js
@@ -1,55 +1,41 @@
-function commissions(input){
+function commissions(input) {
+  let city = String(input[0]);
+  let sales = Number(input[1]);
 
-    let city = String(input[0])
-    let sales = Number(input[1])
-
-    if(city == "Sofia") {
-        if(sales >= 0 && sales <= 500) {
-            sales *= 0.05 
-        }else if (sales > 500 && sales <= 1000) {
-            sales *= 0.07
-        }else if (sales > 1000 && sales <= 10000){
-            sales *= 0.08
-        }else if(sales > 10000) {
-            sales *= 0.12
-        }
-    }else if (city == "Varna") {
-        if(sales >= 0 && sales <= 500) {
-            sales *= 0.045 
-        }else if (sales > 500 && sales <= 1000) {
-            sales *= 0.075
-        }else if (sales > 1000 && sales <= 10000){
-            sales *= 0.10
-        }else if(sales > 10000) {
-            sales *= 0.13
-        }
-
+  if (city == "Sofia") {
+    if (sales >= 0 && sales <= 500) {
+      sales *= 0.05;
+    } else if (sales > 500 && sales <= 1000) {
+      sales *= 0.07;
+    } else if (sales > 1000 && sales <= 10000) {
+      sales *= 0.08;
+    } else if (sales > 10000) {
+      sales *= 0.12;
     }
-    else if (city == "Plovdiv") {
-        if(sales >= 0 && sales <= 500) {
-            sales *= 0.055 
-        }else if (sales > 500 && sales <= 1000) {
-            sales *= 0.08
-        }else if (sales > 1000 && sales <= 10000){
-            sales *= 0.12
-        }else if(sales > 10000) {
-            sales *= 0.145
-        }
-    }else {console.log("error")
+  } else if (city == "Varna") {
+    if (sales >= 0 && sales <= 500) {
+      sales *= 0.045;
+    } else if (sales > 500 && sales <= 1000) {
+      sales *= 0.075;
+    } else if (sales > 1000 && sales <= 10000) {
+      sales *= 0.1;
+    } else if (sales > 10000) {
+      sales *= 0.13;
+    }
+  } else if (city == "Plovdiv") {
+    if (sales >= 0 && sales <= 500) {
+      sales *= 0.055;
+    } else if (sales > 500 && sales <= 1000) {
+      sales *= 0.08;
+    } else if (sales > 1000 && sales <= 10000) {
+      sales *= 0.12;
+    } else if (sales > 10000) {
+      sales *= 0.145;
+    }
+  } else {
+    console.log("error");
+  }
+  console.log(sales.toFixed(2));
 }
-console.log(sales.toFixed(2))
-    }
-    
-    
-       
 
-    
-
-
-   
-
-commissions(["SBourgas",
-"1500"])
-
-
-
+commissions(["SBourgas", "1500"]);
