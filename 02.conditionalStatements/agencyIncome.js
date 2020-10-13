30c4e2ac354ed22c4da5c436c63c6665c4ee14dc
Hasan Hasanov <hasanovh14@gmail.com>
2020-10-13 20:21:55 +0200

    Conditional Statements

--- 02.conditionalStatements/agencyIncome.js
+++ 02.conditionalStatements/agencyIncome.js
@@ -1,31 +1,26 @@
 function income(input) {
+  let avioAgency = input[0];
+  let ticketCountAdults = Number(input[1]);
+  let ticketCountKids = Number(input[2]);
+  let netTicketPriceAdults = Number(input[3]);
+  let serivePrice = Number(input[4]);
 
-    let avioAgency = input[0]
-    let ticketCountAdults = Number(input[1])
-    let ticketCountKids = Number(input[2])
-    let netTicketPriceAdults = Number(input[3])
-    let serivePrice = Number(input[4])
-    
+  let netTicketPriceKids = netTicketPriceAdults * 0.3;
 
-    let netTicketPriceKids = netTicketPriceAdults * 0.30
-    
-    let totalAdultsPrice = netTicketPriceAdults + serivePrice
-    
-    let totalKidsPrice = netTicketPriceKids + serivePrice
-    
+  let totalAdultsPrice = netTicketPriceAdults + serivePrice;
 
-    let totalTicketsPrice = (ticketCountKids * totalKidsPrice) + (ticketCountAdults * totalAdultsPrice)
+  let totalKidsPrice = netTicketPriceKids + serivePrice;
 
-    let profit = totalTicketsPrice * 0.20
-   
+  let totalTicketsPrice =
+    ticketCountKids * totalKidsPrice + ticketCountAdults * totalAdultsPrice;
 
-    console.log(`The profit of your agency from ${avioAgency} tickets is ${profit.toFixed(2)} lv.`)
+  let profit = totalTicketsPrice * 0.2;
 
-
+  console.log(
+    `The profit of your agency from ${avioAgency} tickets is ${profit.toFixed(
+      2
+    )} lv.`
+  );
 }
 
-income(["WizzAir",
-"15",
-"5",
-"120",
-"40"])
+income(["WizzAir", "15", "5", "120", "40"]);
