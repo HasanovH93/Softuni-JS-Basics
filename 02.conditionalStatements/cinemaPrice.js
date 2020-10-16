5844d000e295b1d5da474cfd1dd920bcf2f28463
Hasan Hasanov <hasanovh14@gmail.com>
2020-10-16 20:21:55 +0200

    Conditional Statements

--- 02.conditionalStatements/cinemaPrice.js
+++ 02.conditionalStatements/cinemaPrice.js
@@ -1,30 +1,22 @@
 function cinemaPrice(input) {
+  let projectType = String(input[0]);
+  let countRows = Number(input[1]);
+  let countCows = Number(input[2]);
 
-    let projectType = String(input[0])
-    let countRows = Number(input[1])
-    let countCows = Number(input[2])
+  let premierPrice = 12.0;
+  let normalPrice = 7.5;
+  let discountPrice = 5.0;
+  let totalPrice = 0;
 
-    let premierPrice = 12.00
-    let normalPrice = 7.50
-    let discountPrice = 5.00
-    let totalPrice = 0;
+  if (projectType == "Premiere") {
+    totalPrice = countCows * countRows * premierPrice;
+  } else if (projectType == "Normal") {
+    totalPrice = countCows * countRows * normalPrice;
+  } else if (projectType == "Discount") {
+    totalPrice = countCows * countRows * discountPrice;
+  }
 
-
-    if(projectType == "Premiere") {
-
-       totalPrice = countCows * countRows * premierPrice
-
-      }else if (projectType == "Normal") {
-           totalPrice = countCows * countRows * normalPrice
-      }else if (projectType == "Discount") {
-          totalPrice = countCows * countRows * discountPrice
-      }
-
-
-console.log(totalPrice)
-
+  console.log(totalPrice);
 }
 
-cinemaPrice(["Premiere",
-"10",
-"12"])
+cinemaPrice(["Premiere", "10", "12"]);
