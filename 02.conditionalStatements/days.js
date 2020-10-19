409a364ab3a02367bdc8ea4431091a12c4e07948
Hasan Hasanov <hasanovh14@gmail.com>
2020-10-19 20:21:55 +0200

    Conditional Statements

--- 02.conditionalStatements/days.js
+++
@@ -1,4 +0,0 @@
-function days(input){
-    s
-
-}
--- 02.conditionalStatements/hotelRoom.js
+++ 02.conditionalStatements/hotelRoom.js
@@ -1,43 +1,37 @@
 function hotelRoomPrice(input) {
-    let month = input[0]
-    let countNights = Number(input[1])
+  let month = input[0];
+  let countNights = Number(input[1]);
 
-    let apartmentPrice = 0;
-    let studioPrice = 0;
+  let apartmentPrice = 0;
+  let studioPrice = 0;
 
-    if(month == "May" || month == "October") {
-        studioPrice = 50
-        apartmentPrice = 65
-        if(countNights > 14) {
-            studioPrice = studioPrice * 0.70
-          
-        }else if(countNights > 7) {
-            studioPrice = studioPrice * 0.95
-        }
-    }else if(month == "June" || month == "September") {
-        studioPrice = 75.20
-        apartmentPrice = 68.70
-        if(countNights > 14) {
-
-            studioPrice = studioPrice * 0.80
-            
-        }
-    }else if(month == "July" || month == "August") {
-        studioPrice = 76
-        apartmentPrice = 77
-       
+  if (month == "May" || month == "October") {
+    studioPrice = 50;
+    apartmentPrice = 65;
+    if (countNights > 14) {
+      studioPrice = studioPrice * 0.7;
+    } else if (countNights > 7) {
+      studioPrice = studioPrice * 0.95;
     }
-    if(countNights > 14){
-        apartmentPrice = apartmentPrice * 0.90
+  } else if (month == "June" || month == "September") {
+    studioPrice = 75.2;
+    apartmentPrice = 68.7;
+    if (countNights > 14) {
+      studioPrice = studioPrice * 0.8;
     }
-    
-    let totalPriceStudio = countNights * studioPrice
-    let totalPriceApartment = countNights * apartmentPrice
+  } else if (month == "July" || month == "August") {
+    studioPrice = 76;
+    apartmentPrice = 77;
+  }
+  if (countNights > 14) {
+    apartmentPrice = apartmentPrice * 0.9;
+  }
 
-    console.log(`Apartment: ${totalPriceApartment.toFixed(2)} lv.`)
-    console.log(`Studio: ${totalPriceStudio.toFixed(2)} lv.`)
+  let totalPriceStudio = countNights * studioPrice;
+  let totalPriceApartment = countNights * apartmentPrice;
 
+  console.log(`Apartment: ${totalPriceApartment.toFixed(2)} lv.`);
+  console.log(`Studio: ${totalPriceStudio.toFixed(2)} lv.`);
 }
 
-hotelRoomPrice(["May",
-"15"])
+hotelRoomPrice(["May", "15"]);
