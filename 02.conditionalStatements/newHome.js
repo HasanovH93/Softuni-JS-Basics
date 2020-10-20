2b88b0213eeb4515ddd0af94d83b9ad36b0ca5a0
Hasan Hasanov <hasanovh14@gmail.com>
2020-10-20 20:21:55 +0200

    Conditional Statements

--- 02.conditionalStatements/newHome.js
+++ 02.conditionalStatements/newHome.js
@@ -8,53 +8,51 @@ function newHomeFlowersPrice(input) {
 
   switch (flowersType) {
     case "Roses":
-        totalSum = 5 * flowersCount
-        if(flowersCount > 80) {
-            totalSum = totalSum * 0.9
-        }
-        break;
-    case "Dahlias":
-        totalSum = 3.80 * flowersCount
-        if(flowersCount > 90) {
-            totalSum = totalSum * 0.85
-        }
-        break;
-    case "Tulips":
-        totalSum = 2.80 * flowersCount
-        if(flowersCount > 80) {
-            totalSum = totalSum * 0.85
-        }
-        break;
-    case "Narcissus":
-        totalSum = 3 * flowersCount
-        if(flowersCount < 120) {
-            totalSum = totalSum * 1.15
-        }
-        break;
-    case "Gladiolus":
-        totalSum = 2.50 * flowersCount
-        if(flowersCount < 80) {
-            totalSum = totalSum * 1.20
-        }
-       
+      totalSum = 5 * flowersCount;
+      if (flowersCount > 80) {
+        totalSum = totalSum * 0.9;
+      }
       break;
-    
-   
+    case "Dahlias":
+      totalSum = 3.8 * flowersCount;
+      if (flowersCount > 90) {
+        totalSum = totalSum * 0.85;
+      }
+      break;
+    case "Tulips":
+      totalSum = 2.8 * flowersCount;
+      if (flowersCount > 80) {
+        totalSum = totalSum * 0.85;
+      }
+      break;
+    case "Narcissus":
+      totalSum = 3 * flowersCount;
+      if (flowersCount < 120) {
+        totalSum = totalSum * 1.15;
+      }
+      break;
+    case "Gladiolus":
+      totalSum = 2.5 * flowersCount;
+      if (flowersCount < 80) {
+        totalSum = totalSum * 1.2;
+      }
+
+      break;
   }
-  if(budget >= totalSum) {
-    
-    let moneyLeft = budget - totalSum
-    console.log(`Hey, you have a great garden with ${flowersCount} ${flowersType} and ${moneyLeft.toFixed(2)} leva left.`)
-  }else { 
-      let neededMoney = totalSum - budget
-      console.log(`Not enough money, you need ${neededMoney.toFixed(2)} leva more.`)
+  if (budget >= totalSum) {
+    let moneyLeft = budget - totalSum;
+    console.log(
+      `Hey, you have a great garden with ${flowersCount} ${flowersType} and ${moneyLeft.toFixed(
+        2
+      )} leva left.`
+    );
+  } else {
+    let neededMoney = totalSum - budget;
+    console.log(
+      `Not enough money, you need ${neededMoney.toFixed(2)} leva more.`
+    );
   }
 }
 
-newHomeFlowersPrice(["Tulips",
-"88",
-"260"])
-newHomeFlowersPrice(["Roses",
-"55",
-"250"])
-
+newHomeFlowersPrice(["Tulips", "88", "260"]);
+newHomeFlowersPrice(["Roses", "55", "250"]);
