c4df43d05bb5f9c5bf3e015163cd1676c1d18959
Hasan Hasanov <hasanovh14@gmail.com>
2020-10-24 20:21:55 +0200

    Conditional Statements

--- 02.conditionalStatements/secretPasword.js
+++ 02.conditionalStatements/secretPasword.js
@@ -1,13 +1,11 @@
-function solve(input){
+function solve(input) {
+  let password = "s3cr3t!P@ssw0rd";
 
-    let password = "s3cr3t!P@ssw0rd";
-
-    if(input == password) {
-
-        console.log("Welcome")
-    }else {
-        console.log("Wrong password!")
-    }
+  if (input == password) {
+    console.log("Welcome");
+  } else {
+    console.log("Wrong password!");
+  }
 }
 
-solve("s3cr3t!P@ssw0rdd")
+solve("s3cr3t!P@ssw0rdd");
