4bd7d008d1496503ff6b7cca799f6df98d25626b
Hasan Hasanov <hasanovh14@gmail.com>
2020-10-25 20:21:55 +0200

    Conditional Statements

--- 02.conditionalStatements/skiTrip.js
+++ 02.conditionalStatements/skiTrip.js
@@ -1,52 +1,40 @@
 function skiTripPrice(input) {
+  let daysToStay = Number(input[0]);
+  let roomType = input[1];
+  let review = input[2];
 
-    let daysToStay = Number(input[0])
-    let roomType = input[1]
-    let review =  input[2]
+  let roomPrice = 0;
+  let nightsToStay = daysToStay - 1;
 
-    let roomPrice = 0;
-    let nightsToStay = daysToStay - 1
-    
-    if(roomType == "room for one person") {
-        
-        roomPrice = 18.00 * nightsToStay
-    }else if (roomType == "apartment") {
-        roomPrice = nightsToStay * 25
-        if(nightsToStay < 10) {
-            roomPrice = roomPrice * 0.70
-           
-        }else if (nightsToStay <= 15) {
-            roomPrice = roomPrice * 0.65
-        }else if(nightsToStay > 15){
-            roomPrice = roomPrice * 0.50
-        }
-    }else if (roomType == "president apartment"){
-        roomPrice = 35.00 * nightsToStay
-        if(nightsToStay < 10) {
-            roomPrice = roomPrice * 0.90
-           
-        }else if (nightsToStay <= 15) {
-            roomPrice = roomPrice * 0.85
-        }else if(nightsToStay > 15){
-            roomPrice = roomPrice * 0.80
-        }
+  if (roomType == "room for one person") {
+    roomPrice = 18.0 * nightsToStay;
+  } else if (roomType == "apartment") {
+    roomPrice = nightsToStay * 25;
+    if (nightsToStay < 10) {
+      roomPrice = roomPrice * 0.7;
+    } else if (nightsToStay <= 15) {
+      roomPrice = roomPrice * 0.65;
+    } else if (nightsToStay > 15) {
+      roomPrice = roomPrice * 0.5;
     }
-    if(review == "positive") {
-        roomPrice = roomPrice * 1.25
-       
-    }else if (review == "negative") {
-        roomPrice = roomPrice * 0.90
-       
+  } else if (roomType == "president apartment") {
+    roomPrice = 35.0 * nightsToStay;
+    if (nightsToStay < 10) {
+      roomPrice = roomPrice * 0.9;
+    } else if (nightsToStay <= 15) {
+      roomPrice = roomPrice * 0.85;
+    } else if (nightsToStay > 15) {
+      roomPrice = roomPrice * 0.8;
     }
-    console.log(roomPrice.toFixed(2))
-    
+  }
+  if (review == "positive") {
+    roomPrice = roomPrice * 1.25;
+  } else if (review == "negative") {
+    roomPrice = roomPrice * 0.9;
+  }
+  console.log(roomPrice.toFixed(2));
 }
 
-skiTripPrice(["14",
-"apartment",
-"positive"])
+skiTripPrice(["14", "apartment", "positive"]);
 
-skiTripPrice(["30",
-"president apartment",
-"negative"])
-
+skiTripPrice(["30", "president apartment", "negative"]);
