c3a80a54304f1450c13e1cce0ff54e2a3f79133b
Hasan Hasanov <hasanovh14@gmail.com>
2020-11-13 13:21:43 +0200

    For Loops

--- 03.forLoops/demo.js
+++ 03.forLoops/demo.js
@@ -1,10 +1,8 @@
+var countBits = function (n) {
+  var result =
+    n.toString(2).match(/1/g) != null ? n.toString(2).match(/1/g).length : 0;
+  return result;
+};
+console.log(countBits("a"));
 
-    var countBits = function(n) {
-        var result = n.toString(2).match(/1/g) != null ? n.toString(2).match(/1/g).length : 0;
-        return result;
-      };
-      console.log(countBits('a'))
-
-
-countBits([1234])
-
+countBits([1234]);
