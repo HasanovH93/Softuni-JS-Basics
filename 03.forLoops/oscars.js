c4c54fa3e8394a12379fcb9c2116fe6417ebd3d1
Hasan Hasanov <hasanovh14@gmail.com>
2020-11-18 13:21:43 +0200

    For Loops

--- 03.forLoops/oscars.js
+++ 03.forLoops/oscars.js
@@ -1,42 +1,41 @@
-function oscars(input){
-    
-    let name = input[0];
-    let startPoints = Number(input[1])
-    let inputL = input.length
-  
-   let points = 0;
-    for(let i = 3; i< inputL; i++){
-       if(i % 2 !== 0) {
-          points = i + 1;
-           let total = input[i].length * Number(input[points] / 2)
-           startPoints += total
-           
+function oscars(input) {
+  let name = input[0];
+  let startPoints = Number(input[1]);
+  let inputL = input.length;
 
-         
-       }
-       if(startPoints> 1250.5){
-           break;
-       }
+  let points = 0;
+  for (let i = 3; i < inputL; i++) {
+    if (i % 2 !== 0) {
+      points = i + 1;
+      let total = input[i].length * Number(input[points] / 2);
+      startPoints += total;
     }
-       if(startPoints < 1250.5){
-
-        let pointsNeeded = 1250.5 - startPoints;
-        console.log(`Sorry, ${name} you need ${pointsNeeded.toFixed(1)} more!`)
-       }else {
-           console.log(`Congratulations, ${name} got a nominee for leading role with ${startPoints.toFixed(1)}!`)
-       }
+    if (startPoints > 1250.5) {
+      break;
     }
-  
+  }
+  if (startPoints < 1250.5) {
+    let pointsNeeded = 1250.5 - startPoints;
+    console.log(`Sorry, ${name} you need ${pointsNeeded.toFixed(1)} more!`);
+  } else {
+    console.log(
+      `Congratulations, ${name} got a nominee for leading role with ${startPoints.toFixed(
+        1
+      )}!`
+    );
+  }
+}
 
-
-oscars(["Zahari Baharov",
-"205",
-"4",
-"Johnny Depp",
-"45",
-"Will Smith",
-"29",
-"Jet Lee",
-"10",
-"Matthew Mcconaughey",
-"39"])
+oscars([
+  "Zahari Baharov",
+  "205",
+  "4",
+  "Johnny Depp",
+  "45",
+  "Will Smith",
+  "29",
+  "Jet Lee",
+  "10",
+  "Matthew Mcconaughey",
+  "39",
+]);
