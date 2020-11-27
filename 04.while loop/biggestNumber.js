832767e555ff0eaa51d8403c9c1ac0d406543e08
Hasan Hasanov <hasanovh14@gmail.com>
2020-11-27 13:21:43 +0200

    While Loops

--- 04.while loop/biggestNumber.js
+++ 04.while loop/biggestNumber.js
@@ -1,25 +1,18 @@
-function biggestNumber(input){
+function biggestNumber(input) {
+  let index = 0;
+  let command = input[index++];
 
-    let index = 0;
-    let command = input[index++];
+  let maxNumber = Number.MIN_SAFE_INTEGER;
 
-    let maxNumber = Number.MIN_SAFE_INTEGER;
+  while (command !== "Stop") {
+    let num = Number(command);
 
-    while(command !== "Stop"){
-      
-        let num = Number(command);
-
-        if(maxNumber < num){
-            maxNumber = num;
-        }
-     command = input[index++];
+    if (maxNumber < num) {
+      maxNumber = num;
     }
-    console.log(maxNumber)
-
+    command = input[index++];
+  }
+  console.log(maxNumber);
 }
 
-biggestNumber(["-10",
-"-20",
-"-30",
-"Stop"])
-
+biggestNumber(["-10", "-20", "-30", "Stop"]);
