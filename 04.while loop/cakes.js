e1fedccc9f14aee3a095759ebdf5624091237779
Hasan Hasanov <hasanovh14@gmail.com>
2020-11-29 13:21:43 +0200

    While Loops

--- 04.while loop/cakes.js
+++ 04.while loop/cakes.js
@@ -1,34 +1,28 @@
-function cakes(input){
+function cakes(input) {
+  let width = Number(input[0]);
+  let height = Number(input[1]);
+  let countPieces = width * height;
+  let piecesTaken = 0;
+  let command = input[2];
 
-    let width = Number(input[0]);
-    let height = Number(input[1]);
-    let countPieces = width * height;
-    let piecesTaken = 0;
-    let command = input[2]
-    
-    let index = 3;
+  let index = 3;
 
-    while(command !== "STOP" ){
-        piecesTaken += Number(command);
+  while (command !== "STOP") {
+    piecesTaken += Number(command);
 
-        if(piecesTaken > countPieces) {
-            break;
-        }
-        command = input[index]
-        index++;
-       
-    }
     if (piecesTaken > countPieces) {
-        console.log(`No more cake left! You need ${piecesTaken - countPieces } pieces more.`);
-    } else {
-       console.log(`${countPieces - piecesTaken} pieces are left.`); 
+      break;
     }
+    command = input[index];
+    index++;
+  }
+  if (piecesTaken > countPieces) {
+    console.log(
+      `No more cake left! You need ${piecesTaken - countPieces} pieces more.`
+    );
+  } else {
+    console.log(`${countPieces - piecesTaken} pieces are left.`);
+  }
 }
 
-cakes(["10",
-"10",
-"20",
-"20",
-"20",
-"20",
-"21"])
+cakes(["10", "10", "20", "20", "20", "20", "21"]);
