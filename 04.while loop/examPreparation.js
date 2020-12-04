f717b40d86f6a65551402f59b4c7ffb460012ff2
Hasan Hasanov <hasanovh14@gmail.com>
2020-12-04 13:21:43 +0200

    While Loops

--- 04.while loop/examPreparation.js
+++ 04.while loop/examPreparation.js
@@ -6,45 +6,40 @@ function examPreparation(input) {
   let counter = 0;
   let sumGrade = 0;
   let problemsCounter = 0;
-  let name = ''
+  let name = "";
   let needBreak = false;
   while (command !== "Enough") {
-      problemsCounter++;
-     name = command;
+    problemsCounter++;
+    name = command;
     let grade = Number(input[index++]);
     if (grade <= 4.0) {
-        
       counter++;
-      
-  }
-  if (counter >= negativeGrade) {
-      needBreak = true;
-    console.log(`You need a break, ${counter} poor grades.`);
-    break;
-  }
-  sumGrade += grade;
-  command = input[index++];
-  
     }
-    if(!needBreak) {
-        let avgGrade = sumGrade / problemsCounter;
+    if (counter >= negativeGrade) {
+      needBreak = true;
+      console.log(`You need a break, ${counter} poor grades.`);
+      break;
+    }
+    sumGrade += grade;
+    command = input[index++];
+  }
+  if (!needBreak) {
+    let avgGrade = sumGrade / problemsCounter;
     console.log(`Average score: ${avgGrade.toFixed(2)}`);
     console.log(`Number of problems: ${problemsCounter}`);
-    console.log(`Last problem: ${name}`)
-
-    }
-    
-
+    console.log(`Last problem: ${name}`);
+  }
 }
 
-examPreparation(["3",
-"Money",
-"6",
-"Story",
-"4",
-"Spring Time",
-"5",
-"Bus",
-"6",
-"Enough"])
-
+examPreparation([
+  "3",
+  "Money",
+  "6",
+  "Story",
+  "4",
+  "Spring Time",
+  "5",
+  "Bus",
+  "6",
+  "Enough",
+]);
