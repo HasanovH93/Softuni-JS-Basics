5865cd2605d6a4a35c44d3d4da3cdadcbba1ad47
Hasan Hasanov <hasanovh14@gmail.com>
2020-12-09 13:21:43 +0200

    While Loops

--- 02.conditionalStatements/3.timePlus15.js
+++ 02.conditionalStatements/3.timePlus15.js
@@ -1,30 +1,21 @@
 function time(input) {
+  let hours = Number(input[0]);
+  let minutes = Number(input[1]);
 
-    let hours = Number(input[0])
-    let minutes = Number(input[1])
+  let mins = minutes + 15;
 
-    let mins = minutes + 15
-
-    if(mins > 59) {
-        hours ++
-        mins -= 60
-    } 
-    if(hours > 23) {
-        hours = 0
-    }
-    if(mins < 10) {
-        console.log(`${hours}:0${mins}`)
-    }else {
-        console.log(`${hours}:${mins}`)
-    }
-    
-   
-
-    
-   
-  
-    
-
+  if (mins > 59) {
+    hours++;
+    mins -= 60;
+  }
+  if (hours > 23) {
+    hours = 0;
+  }
+  if (mins < 10) {
+    console.log(`${hours}:0${mins}`);
+  } else {
+    console.log(`${hours}:${mins}`);
+  }
 }
 
-time([1,46])
+time([1, 46]);
--- 04.while loop/oldBooks.js
+++ 04.while loop/oldBooks.js
@@ -1,29 +1,22 @@
-function oldBooks(input){
+function oldBooks(input) {
+  let favBook = input[0];
+  let index = 1;
+  let bookIsFound = false;
+  let nextBookName = input[index];
 
-    let favBook = input[0];
-    let index = 1;
-    let bookIsFound = false;
-    let nextBookName = input[index];
-
-    while(nextBookName !== "No More Books"){
-        if(nextBookName === favBook){
-            bookIsFound = true;
-            break;
-        }
-        index++;
-        nextBookName = input[index]
+  while (nextBookName !== "No More Books") {
+    if (nextBookName === favBook) {
+      bookIsFound = true;
+      break;
     }
-    if(bookIsFound === false){
-        console.log("The book you search is not here!");
-        console.log(`You checked ${index -1} books.`)
-    }else {
-        console.log(`You checked ${index - 1} books and found it.`)
-    }
-
-
-
+    index++;
+    nextBookName = input[index];
+  }
+  if (bookIsFound === false) {
+    console.log("The book you search is not here!");
+    console.log(`You checked ${index - 1} books.`);
+  } else {
+    console.log(`You checked ${index - 1} books and found it.`);
+  }
 }
-oldBooks(["Troy",
-"Stronger",
-"Life Style",
-"Troy"])
+oldBooks(["Troy", "Stronger", "Life Style", "Troy"]);
