d2e5763909fc09cbb8e76418d1cff6a508972d4f
Hasan Hasanov <hasanovh14@gmail.com>
2020-12-11 13:21:43 +0200

    While Loops

--- 04.while loop/password.js
+++ 04.while loop/password.js
@@ -1,19 +1,13 @@
-function password(input){
-      
-    let index = 0;
-    let name = input[index++];
-    let pass = input[index++];
-    
-    let tempPassword = input[index++];
-    while(tempPassword !== pass){
-        tempPassword = input[index++];
-    }
-    console.log(`Welcome ${name}!`)
-    
+function password(input) {
+  let index = 0;
+  let name = input[index++];
+  let pass = input[index++];
+
+  let tempPassword = input[index++];
+  while (tempPassword !== pass) {
+    tempPassword = input[index++];
+  }
+  console.log(`Welcome ${name}!`);
 }
 
-password(["Nakov",
-"1234",
-"Pass",
-"1324",
-"1234"])
+password(["Nakov", "1234", "Pass", "1324", "1234"]);
