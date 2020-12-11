ceb6afb20c84469ca630676493174163e1bf6671
Hasan Hasanov <hasanovh14@gmail.com>
2020-12-11 13:21:43 +0200

    While Loops

--- 04.while loop/readText.js
+++ 04.while loop/readText.js
@@ -1,22 +1,13 @@
-function readText(input){
-   
-    let sum = 0;
-    let index = 0;
-    let n = Number(input[index++]);
+function readText(input) {
+  let sum = 0;
+  let index = 0;
+  let n = Number(input[index++]);
 
-    while(sum < n) {
-        num = Number(input[index++]);
-        sum+=num
-       
-
-    }
-    console.log(sum)
-        
-    
+  while (sum < n) {
+    num = Number(input[index++]);
+    sum += num;
+  }
+  console.log(sum);
 }
 
-readText(["100",
-"10",
-"20",
-"30",
-"40"])
+readText(["100", "10", "20", "30", "40"]);
