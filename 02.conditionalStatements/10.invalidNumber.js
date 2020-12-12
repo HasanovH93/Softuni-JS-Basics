ec9cc322adb2a611251e5b5853735bc68e24ee5d
Hasan Hasanov <hasanovh14@gmail.com>
2020-12-12 20:21:55 +0200

    Conditional Statements

--- 02.conditionalStatements/10.invalidNumber.js
+++ 02.conditionalStatements/10.invalidNumber.js
@@ -1,11 +1,7 @@
 function invNumber(input) {
-
-    let num = Number(input)
-    if((num < 100 || num > 200)&& (num !== 0)) {
-
-    
-        console.log("invalid")
-    }
-
+  let num = Number(input);
+  if ((num < 100 || num > 200) && num !== 0) {
+    console.log("invalid");
+  }
 }
-invNumber([0])
+invNumber([0]);
