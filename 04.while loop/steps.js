30201c1ba69fd4e8da4321ff4cb1fbff8ea01a99
Hasan Hasanov <hasanovh14@gmail.com>
2020-12-17 13:21:43 +0200

    While Loops

--- 04.while loop/steps.js
+++ 04.while loop/steps.js
@@ -1,41 +1,31 @@
-function steps(input){
+function steps(input) {
+  let target = 10000;
+  let sum = 0;
+  let index = 0;
+  let command = input[index];
 
-     let target = 10000;
-     let sum = 0;
-     let index = 0;
-     let command = input[index];
+  while (command !== "Going home") {
+    let steps = Number(command);
+    sum += steps;
 
-     while(command !== "Going home"){
-
-        let steps = Number(command);
-        sum += steps;
-        
-        if(sum >= target){
-           
-            console.log("Goal reached! Good job!");
-            console.log(`${sum - target} steps over the goal!`);
-            break;
-        }
-        index++; target
-        command = input[index]
-     }
-     if(command === "Going home"){
-         let stepsToHome = Number(input[index + 1]);
-         sum += stepsToHome;
-         if(sum >= target){
-            console.log("Goal reached! Good job!");
-            console.log(`${sum - target} steps over the goal!`);
-           
-         }else {
-             console.log(`${target - sum} more steps to reach goal.`);
-         }
-     }
-
+    if (sum >= target) {
+      console.log("Goal reached! Good job!");
+      console.log(`${sum - target} steps over the goal!`);
+      break;
+    }
+    index++;
+    target;
+    command = input[index];
+  }
+  if (command === "Going home") {
+    let stepsToHome = Number(input[index + 1]);
+    sum += stepsToHome;
+    if (sum >= target) {
+      console.log("Goal reached! Good job!");
+      console.log(`${sum - target} steps over the goal!`);
+    } else {
+      console.log(`${target - sum} more steps to reach goal.`);
+    }
+  }
 }
-steps(["1500",
-"300",
-"2500",
-"3000",
-"Going home",
-"200"])
-
+steps(["1500", "300", "2500", "3000", "Going home", "200"]);
