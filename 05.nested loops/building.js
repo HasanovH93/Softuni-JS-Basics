ade53e74f53579686d366440198781af0abd743b
Hasan Hasanov <hasanovh14@gmail.com>
2020-12-20 13:21:43 +0200

    While Loops

--- 05.nested loops/building.js
+++ 05.nested loops/building.js
@@ -1,22 +1,19 @@
-function building(input){
+function building(input) {
+  let floorCount = Number(input[0]);
+  let rooms = Number(input[1]);
 
-    let floorCount = Number(input[0]);
-    let rooms = Number(input[1]);
-
-    for(let a = floorCount; a > 0; a--){
-        let buff= "";
-        for(let b = 0; b < rooms; b++){
-            if(a === floorCount) {
-                buff += `L${a}${b} `
-            }else if ( a % 2 === 0){
-                buff += `O${a}${b} `
-            }else {
-                buff += `A${a}${b} `
-            }
-         
-        }
-        console.log(buff)
+  for (let a = floorCount; a > 0; a--) {
+    let buff = "";
+    for (let b = 0; b < rooms; b++) {
+      if (a === floorCount) {
+        buff += `L${a}${b} `;
+      } else if (a % 2 === 0) {
+        buff += `O${a}${b} `;
+      } else {
+        buff += `A${a}${b} `;
+      }
     }
-          
+    console.log(buff);
+  }
 }
-building(["6", "4"])
+building(["6", "4"]);
