2c82c08cbbf6cb1ef6aa22deab89347faa4c0215
Hasan Hasanov <hasanovh14@gmail.com>
2020-12-20 13:21:43 +0200

    While Loops

--- 04.while loop/vacation.js
+++ 04.while loop/vacation.js
@@ -1,56 +1,53 @@
-function vacation(input){
+function vacation(input) {
+  let neededMoney = Number(input[0]);
+  let availableMoney = Number(input[1]);
 
-    let neededMoney = Number(input[0]);
-    let availableMoney = Number(input[1]);
+  let index = 2;
+  let command = input[index];
+  let spendDays = 0;
+  let totalDays = 0;
 
-    let index = 2;
-    let command = input[index]
-    let spendDays =0;
-    let totalDays = 0;
+  while (availableMoney < neededMoney) {
+    totalDays++;
+    index++;
 
-    while(availableMoney < neededMoney) {
-        totalDays++;
-        index++;
-
-        if(command === "spend"){
-            spendDays++;
-            if(spendDays === 5){
-                console.log("You can't save the money.");
-                console.log(totalDays);
-                break;
-            }
-            let moneyToSpend = Number(input[index]);
-            availableMoney -= moneyToSpend;
-            if(availableMoney < 0){
-                availableMoney = 0;
-            }
-
-        }else if(command === "save"){
-            spendDays = 0;
-            let moneyToSave = Number(input[index]);
-            availableMoney += moneyToSave;
-        }
-        index++;
-        command = input[index];
-
+    if (command === "spend") {
+      spendDays++;
+      if (spendDays === 5) {
+        console.log("You can't save the money.");
+        console.log(totalDays);
+        break;
+      }
+      let moneyToSpend = Number(input[index]);
+      availableMoney -= moneyToSpend;
+      if (availableMoney < 0) {
+        availableMoney = 0;
+      }
+    } else if (command === "save") {
+      spendDays = 0;
+      let moneyToSave = Number(input[index]);
+      availableMoney += moneyToSave;
     }
-    
-    if(availableMoney >= neededMoney) {
-        console.log(`You saved the money for ${totalDays} days.`);
-    }
-    
+    index++;
+    command = input[index];
+  }
+
+  if (availableMoney >= neededMoney) {
+    console.log(`You saved the money for ${totalDays} days.`);
+  }
 }
 
-vacation(["110",
-"60",
-"spend",
-"10",
-"spend",
-"10",
-"spend",
-"10",
-"spend",
-"10",
-"spend",
-"10"])	
-
+vacation([
+  "110",
+  "60",
+  "spend",
+  "10",
+  "spend",
+  "10",
+  "spend",
+  "10",
+  "spend",
+  "10",
+  "spend",
+  "10",
+]);
