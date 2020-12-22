459620a17ab347afa18a3020559f78a29b551afe
Hasan Hasanov <hasanovh14@gmail.com>
2020-12-22 13:21:43 +0200

    While Loops

--- 05.nested loops/clock.js
+++ 05.nested loops/clock.js
@@ -1,9 +1,8 @@
 function clock() {
-
-    for(let h = 1; h < 60; h++){
-        for(let m = 0; m < 60; m++) {
-            console.log(`${h}:${m}`)
-        }
+  for (let h = 1; h < 60; h++) {
+    for (let m = 0; m < 60; m++) {
+      console.log(`${h}:${m}`);
     }
+  }
 }
 clock();
