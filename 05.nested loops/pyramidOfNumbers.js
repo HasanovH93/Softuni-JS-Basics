cc4e63dfaca6ed79aa302502eac716e8d2fb9410
Hasan Hasanov <hasanovh14@gmail.com>
2020-12-29 13:21:43 +0200

    While Loops

--- 05.nested loops/pyramidOfNumbers.js
+++ 05.nested loops/pyramidOfNumbers.js
@@ -1,26 +1,23 @@
-function pyramid(input){
+function pyramid(input) {
+  let n = Number(input[0]);
+  let printNum = 1;
+  let isFinish = false;
+  for (let i = 1; i <= n; i++) {
+    let buff = "";
+    for (let y = 1; y <= i; y++) {
+      buff += printNum + " ";
+      printNum++;
 
-    let n = Number(input[0]);
-    let printNum =1 ;
-    let isFinish = false;
-    for(let i = 1; i <= n; i++) {
-        let buff = "";
-        for(let y = 1; y<=i; y++){
-            buff += printNum + ' ';
-            printNum++;
-
-            if(printNum > n) {
-                isFinish = true;
-                break;
-            }
-
-        }
-        console.log(buff);
-        if(isFinish) {
-            break;
-        }
+      if (printNum > n) {
+        isFinish = true;
+        break;
+      }
     }
-
+    console.log(buff);
+    if (isFinish) {
+      break;
+    }
+  }
 }
 
 pyramid(["7"]);
