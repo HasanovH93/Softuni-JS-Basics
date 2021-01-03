fa99cefb75fd16bcc76bdf11e805180ca709e87f
Hasan Hasanov <hasanovh14@gmail.com>
2021-01-03 18:21:43 +0200

    Exam Exercise

--- 06.Exam Exercise/agencyProfit.js
+++ 06.Exam Exercise/agencyProfit.js
@@ -1,25 +1,22 @@
-function agencyProfit(input){
+function agencyProfit(input) {
+  let name = input.shift();
+  let totalAdultsTicket = Number(input.shift());
+  let totalKidsTicket = Number(input.shift());
+  let ticketPrice = Number(input.shift());
+  let serviceTax = Number(input.shift());
 
-    let name = input.shift();
-    let totalAdultsTicket = Number(input.shift());
-    let totalKidsTicket = Number(input.shift());
-    let ticketPrice = Number(input.shift());
-    let serviceTax = Number(input.shift());
+  let kidsDiscount = ticketPrice * 0.3;
 
-    let kidsDiscount = ticketPrice * 0.30;
+  let AdultsService = ticketPrice + serviceTax;
+  let kidsService = kidsDiscount + serviceTax;
 
-    let AdultsService = ticketPrice + serviceTax;
-    let kidsService = kidsDiscount + serviceTax;
+  let totalPrice =
+    totalKidsTicket * kidsService + totalAdultsTicket * AdultsService;
 
-    let totalPrice = (totalKidsTicket *  kidsService) + (totalAdultsTicket * AdultsService)
+  let profit = totalPrice * 0.2;
 
-    let profit = totalPrice * 0.20;
-
-    console.log(`The profit of your agency from ${name} tickets is ${profit.toFixed(2)} lv.`);
-
+  console.log(
+    `The profit of your agency from ${name} tickets is ${profit.toFixed(2)} lv.`
+  );
 }
-agencyProfit(["WizzAir",
-"15",
-"5",
-"120",
-"40"])
+agencyProfit(["WizzAir", "15", "5", "120", "40"]);
