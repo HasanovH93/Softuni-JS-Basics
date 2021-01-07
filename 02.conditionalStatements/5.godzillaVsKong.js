a26f4d48903d17667ccbfb65209794ada58bcbb9
Hasan Hasanov <hasanovh14@gmail.com>
2021-01-07 18:21:43 +0200

    Exam Exercise

--- 02.conditionalStatements/5.godzillaVsKong.js
+++ 02.conditionalStatements/5.godzillaVsKong.js
@@ -1,41 +1,30 @@
 function movie(input) {
-    let budget = Number(input.shift());
-    let staff = Number(input.shift());
-    let clothesPriceForOne = Number(input.shift());
+  let budget = Number(input.shift());
+  let staff = Number(input.shift());
+  let clothesPriceForOne = Number(input.shift());
 
-    let decor = budget * 0.10;
-    let totalPriceClothes = staff * clothesPriceForOne;
+  let decor = budget * 0.1;
+  let totalPriceClothes = staff * clothesPriceForOne;
 
-    if(staff > 150){ 
+  if (staff > 150) {
+    totalPriceClothes = totalPriceClothes - totalPriceClothes * 0.1;
+  }
 
-        totalPriceClothes = totalPriceClothes - totalPriceClothes * 0.10; 
-    }
+  let totalPrice = decor + totalPriceClothes;
 
-    let totalPrice = decor + totalPriceClothes
-    
-    let totalBudget = Math.abs(budget - totalPrice)
-    
-    
+  let totalBudget = Math.abs(budget - totalPrice);
 
-    if(totalPrice > budget) {
-       
-        console.log("Not enough money!")
-        console.log(`Wingard needs ${totalBudget.toFixed(2)} leva more.`)
-    }else {
-        console.log("Action!" )
-        console.log(`Wingard starts filming with ${totalBudget.toFixed(2)} leva left.`)	
-
-    }
-
-
-
+  if (totalPrice > budget) {
+    console.log("Not enough money!");
+    console.log(`Wingard needs ${totalBudget.toFixed(2)} leva more.`);
+  } else {
+    console.log("Action!");
+    console.log(
+      `Wingard starts filming with ${totalBudget.toFixed(2)} leva left.`
+    );
+  }
 }
 
-movie(["20000",
-"120",
-"55.5"])
+movie(["20000", "120", "55.5"]);
 
-movie(["9587.88",
-"222",
-"55.68"])
-
+movie(["9587.88", "222", "55.68"]);
